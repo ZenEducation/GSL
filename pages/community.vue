3e50dfb67dee07d6e4592fb1079c505c0dd9e62d
mohit soni <imohitsoni28@gmail.com>
2023-05-11 00:30:43 +0530

    Communnity page ui implimented V1

---
+++ pages/community.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- <Navbar/> -->
+        <breadcrumb/>
+        <commusection/>
+        <NewsLatter/>
+        
+    </div>
+</template>
+
+<script setup>
+    
+    import breadcrumb from '@/components/Community/breadcrumb.vue'
+    import commusection from '@/components/Community/commuSection.vue'
+    
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>

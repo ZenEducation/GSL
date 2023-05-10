9e9f8dcdec3104b36f74f70ab647227b3dff5de6
mohit soni <imohitsoni28@gmail.com>
2023-05-11 00:43:47 +0530

    membership wo dark mode

---
+++ pages/membership.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    
+        <breadcrumb/>
+        <member/>
+        <faq/>
+        <NewsLatter/>
+        
+    </div>
+</template>
+
+<script setup>
+    
+    import breadcrumb from '@/components/Membership/breadcrumb.vue'
+    import member from '@/components/Membership/memberSection.vue'
+    import faq from '@/components/Membership/faq.vue'
+
+</script>
+
+<style scoped>
+
+</style>

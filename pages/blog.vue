fb8f37fc9386fa450d05ac35e662dbcdbd712698
mohit soni <imohitsoni28@gmail.com>
2023-05-12 00:06:26 +0530

    blog single

---
+++ pages/blog.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Navbar/>
+        <breadcrumb/>
+        <content/>
+        <NewsLatter/>
+    </div>
+</template>
+
+<script setup>
+import breadcrumb from '~~/components/blog/breadcrumb.vue';
+import content from '~~/components/blog/content.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>

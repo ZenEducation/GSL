7d11251d5caec47a97b91e41783ea824514090f4
mohit soni <imohitsoni28@gmail.com>
2023-05-11 18:59:31 +0530

    singleProfile ui updated

--- components/singleProfile/profile.vue
+++ components/singleProfile/profile.vue
@@ -5,8 +5,10 @@ import profile_aside_area from './profile_aside_area.vue';
 import profile_section from './profile_section.vue';
 import friend_section from './friend_section.vue';
 
-console.log("object");
-
+const currentSection = ref('activity');
+const changeSection = ( update )=>{
+    currentSection.value = update;
+}
 
 </script>
 
@@ -22,17 +24,17 @@ console.log("object");
                     <div class="xl:w-2/5 lg:w-1/2 pr-4 pl-4 mb-30">
                         <div class="profile-main-content">
                             <ul class="bg-white shadow-[0px_5px_30px_0px_rgba(35,57,184,0.2)] block text-center mb-px pt-1.5 pb-2.5 px-2.5 rounded-[10px]">
-                                <li  class="inline-block px-0 py-[5px] list-none">
+                                <li @click="()=>{ changeSection('activity') }" class="inline-block px-0 py-[5px] list-none">
                                     <div  class=" cursor-pointer active text-[#333333] text-base leading-[26px] font-bold relative transition-all duration-[ease] delay-[0.3s] bg-transparent no-underline inline-block mx-3 my-0 active:text-[#ac216c] hover:text-[#ac216c]">
                                         Activity
                                     </div>
                                 </li>
-                                <li class="inline-block px-0 py-[5px] list-none">
+                                <li @click="()=>{ changeSection('profile') }" class="inline-block px-0 py-[5px] list-none">
                                     <div  class=" cursor-pointer text-[#333333] text-base leading-[26px] font-bold relative transition-all duration-[ease] delay-[0.3s] bg-transparent no-underline inline-block mx-3 my-0 active:text-[#ac216c] hover:text-[#ac216c]">
                                         Profile
                                     </div>
                                 </li>
-                                <li class="inline-block px-0 py-[5px] list-none">
+                                <li @click="()=>{ changeSection('friends') }" class="inline-block px-0 py-[5px] list-none">
                                     <div   class=" cursor-pointer text-[#333333] text-base leading-[26px] font-bold relative transition-all duration-[ease] delay-[0.3s] bg-transparent no-underline inline-block mx-3 my-0 active:text-[#ac216c] hover:text-[#ac216c]">
                                         Friends
                                         <div class="absolute bg-[-o-linear-gradient(284deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] text-[10px] leading-[10px] text-white right-[-17px] px-[5px] py-[3px] rounded-[30px] -top-1.5">04</div>
@@ -52,9 +54,9 @@ console.log("object");
                                 </li>
                             </ul>
                         </div>
-                        <profile_main_content />
-                        <profile_section />
-                        <friend_section />
+                        <profile_main_content v-if="currentSection == 'activity'" />
+                        <profile_section v-if="currentSection == 'profile'"/>
+                        <friend_section v-if="currentSection == 'friends'"/>
                     </div>
                     <div class="xl:w-1/4 lg:w-2/5  pr-4 pl-4 ">
                         <profile_aside_area/>

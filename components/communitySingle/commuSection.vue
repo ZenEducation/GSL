3ef02e3b60f9707693af26a83ab8e9bb759fdf40
mohit soni <imohitsoni28@gmail.com>
2023-05-11 19:57:29 +0530

    community single V1

---
+++ components/communitySingle/commuSection.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div class="bg-[#4827bb]">
+            <div class=" w-full mx-auto px-[15px] sm:max-w-[540px] sm:px-4 md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1170px] ">
+                <div class="flex flex-wrap ">
+                <div class="lg:w-full pr-4 pl-4">
+                    <ul class="block relative z-[9] ml-[30px] mb-px pl-[25%] pt-1.5 pb-2.5 px-2.5 rounded-[10px]">
+                        <li class="inline-block">
+                            <NuxtLink to="#" class="active text-white text-base leading-[26px] font-bold relative transition-all duration-[ease] delay-[0.3s] mx-3 my-0 active:text-[#ff3366] hover:text-[#ff3366] no-underline">
+                                Activity 
+                            </NuxtLink>
+                        </li>
+                        <li class="inline-block">
+                            <NuxtLink to="#" class="text-white text-base leading-[26px] font-bold relative transition-all duration-[ease] delay-[0.3s] mx-3 my-0 active:text-[#ff3366] hover:text-[#ff3366] no-underline"> 
+                                Profile 
+                            </NuxtLink>
+                        </li>
+                        <li class="inline-block">
+                            <NuxtLink to="#" class="text-white text-base leading-[26px] font-bold relative transition-all duration-[ease] delay-[0.3s] mx-3 my-0 active:text-[#ff3366] hover:text-[#ff3366] no-underline">
+                                Friends  
+                                <div class="absolute bg-[-o-linear-gradient(284deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] text-[10px] leading-[10px] text-white right-[-17px] px-[5px] py-[3px] rounded-[30px] -top-1.5">04</div>
+                            </NuxtLink>
+                        </li>
+                        <li class="inline-block">
+                            <NuxtLink to="#" class="text-white text-base leading-[26px] font-bold relative transition-all duration-[ease] delay-[0.3s] mx-3 my-0 active:text-[#ff3366] hover:text-[#ff3366] no-underline">
+                                Groups 
+                                <div class="absolute bg-[-o-linear-gradient(284deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] text-[10px] leading-[10px] text-white right-[-17px] px-[5px] py-[3px] rounded-[30px] -top-1.5">14</div>
+                            </NuxtLink>
+                        </li>
+                        <li class="inline-block">
+                            <NuxtLink to="#" class="text-white text-base leading-[26px] font-bold relative transition-all duration-[ease] delay-[0.3s] mx-3 my-0 active:text-[#ff3366] hover:text-[#ff3366] no-underline">
+                                Media 
+                                <div class="absolute bg-[-o-linear-gradient(284deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] text-[10px] leading-[10px] text-white right-[-17px] px-[5px] py-[3px] rounded-[30px] -top-1.5">47</div>
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/communitySingle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Navbar/>
+        <breadcrumb/>
+        <communication/>
+        <profile/>
+        <NewsLatter/>
+
+    </div>
+</template>
+
+<script setup>
+import breadcrumb from '@/components/communitySingle/breadcrumb.vue'
+import communication from '@/components/communitySingle/commuSection.vue'
+import profile from '@/components/communitySingle/profileSection.vue'
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>

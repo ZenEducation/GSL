b8b5753eb84ef13b430d6d54057a4132e663178c
mohit soni <imohitsoni28@gmail.com>
2023-05-16 16:48:02 +0530

    login register

---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="dark:bg-[#202342] dark:text-[white] ">
+        <login/>
+    </div>
+</template>
+
+<script setup>
+    import login from '@/components/login/loginSec.vue'
+</script>
+
+<style lang="scss" scoped>
+
+</style>

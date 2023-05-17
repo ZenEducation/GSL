6b494231f3f0ed0711ccf876ac53db81577638ed
mohit soni <imohitsoni28@gmail.com>
2023-05-17 16:28:55 +0530

    quick start , refund , safety , faqs

---
+++ pages/quick_start.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class=" dark:text-[white] dark:bg-[#202342]">
+
+        <Navbar />
+        <Breadcrumb title="Quick Start Guide"/>
+        <Content/>
+        <NewsLatter />
+    </div>
+</template>
+<script setup>
+import Breadcrumb from '~~/components/Breadcrumb.vue';
+import Content from '~~/components/quickStart/content.vue'
+
+</script>
---
+++ pages/refund_policy.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class=" dark:text-[white] dark:bg-[#202342]">
+
+        <Navbar />
+        <Breadcrumb title="Refund Policy"/>
+        <Content/>
+        <NewsLatter />
+    </div>
+</template>
+<script setup>
+import Breadcrumb from '~~/components/Breadcrumb.vue';
+import Content from '~~/components/refund/content.vue'
+
+</script>

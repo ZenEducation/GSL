14cbe248ef6539a291971227dcf65aba4a0aa2c3
mohit soni <imohitsoni28@gmail.com>
2023-05-20 11:20:21 +0530

    banner dark mode update

--- components/Index/Banner.vue
+++ components/Index/Banner.vue
@@ -20,38 +20,37 @@
                                 </p>
                                 <div class="flex items-center">
                                     <div class="mr-3">
-
+                
                                         <input type="radio" name="gender" id="males" checked
                                             class="w-5 h-5 border rounded-[50%] border-solid border-[#5650ce] hover:shadow-[0_0_5px_0px_#5650ce_inset] before:content-[''] before:block before:w-3/5 before:h-3/5 before:mx-auto before:my-[20%] before:rounded-[50%] checked:before:bg-[#5650ce] checked:bg-[#5650ce]"><label
                                             for="males" class=" ml-2">Man</label>
                                     </div>
                                     <div>
-
+                
                                         <input type="radio" name="gender" id="females"
                                             class="w-5 h-5 border  rounded-[50%] border-solid border-[#5650ce] hover:shadow-[0_0_5px_0px_#5650ce_inset] before:content-[''] before:block before:w-3/5 before:h-3/5 before:mx-auto before:my-[20%] before:rounded-[50%] checked:before:bg-[#5650ce] checked:bg-[#5650ce]"><label
                                             for="females" class=" ml-2">Woman</label>
                                     </div>
                                     <div>
-
+                
                                         <input type="radio" name="gender" id="others"
                                             class="ml-2 w-5 h-5 border  rounded-[50%] border-solid border-[#5650ce] hover:shadow-[0_0_5px_0px_#5650ce_inset] before:content-[''] before:block before:w-3/5 before:h-3/5 before:mx-auto before:my-[20%] before:rounded-[50%] checked:before:bg-[#5650ce] checked:bg-[#5650ce]"><label
                                             for="others" class=" ml-2">Other</label>
                                     </div>
                                 </div>
                             </div>
-                            <div
-                                class="flex pt-[22px] pb-[15px] border-b-[rgba(0,0,0,0.1)] border-b border-solid px-5 py-[17px]">
+                            <div class="flex pt-[22px] pb-[15px] border-b-[rgba(0,0,0,0.1)] border-b border-solid px-5 py-[17px]">
                                 <p class="title text-lg leading-7 font-bold relative mr-5 top-[7px]">
                                     Seeking a :
                                 </p>
                                 <div class="flex items-center">
                                     <div class="mr-4">
-
+                
                                         <input type="radio" name="seeking" id="males" checked
                                             class="w-5 h-5 border rounded-[50%] border-solid border-[#5650ce] hover:shadow-[0_0_5px_0px_#5650ce_inset] before:content-[''] before:block before:w-3/5 before:h-3/5 before:mx-auto before:my-[20%] before:rounded-[50%] checked:before:bg-[#5650ce] checked:bg-[#5650ce]"><label
                                             for="males" class=" ml-2">Man</label>
                                     </div>
-
+                
                                     <div>
                                         <input type="radio" name="seeking" id="females"
                                             class="w-5 h-5 border  rounded-[50%] border-solid border-[#5650ce] hover:shadow-[0_0_5px_0px_#5650ce_inset] before:content-[''] before:block before:w-3/5 before:h-3/5 before:mx-auto before:my-[20%] before:rounded-[50%] checked:before:bg-[#5650ce] checked:bg-[#5650ce]"><label
@@ -71,10 +70,10 @@
                                 <div class="flex items-center">
                                     <div class="mr-3">
                                         <select
-                                            class="w-[90px] bg-transparent leading-[26px] border h-auto pl-[15px] pr-[30px] py-0 rounded-[50px] border-solid border-[rgba(0,0,0,0.1)]">
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;" value="">18</option>
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;" value="">20</option>
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;" value="">24</option>
+                                            class="w-[90px] bg-transparent dark:bg-[#262950] dark:text-white leading-[26px] border h-auto pl-[15px] pr-[30px] py-0 rounded-[50px] border-solid border-[rgba(0,0,0,0.1)]">
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;" value="">18</option>
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;" value="">20</option>
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;" value="">24</option>
                                         </select>
                                     </div>
                                     <div>
@@ -82,10 +81,10 @@
                                     </div>
                                     <div class="ml-3">
                                         <select
-                                            class="w-[90px] bg-transparent leading-[26px] border h-auto pl-[15px] pr-[30px] py-0 rounded-[50px] border-solid border-[rgba(0,0,0,0.1)]">
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;" value="">30</option>
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;" value="">35</option>
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;" value="">40</option>
+                                            class="w-[90px] bg-transparent dark:bg-[#262950] dark:text-white leading-[26px] border h-auto pl-[15px] pr-[30px] py-0 rounded-[50px] border-solid border-[rgba(0,0,0,0.1)]">
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;" value="">30</option>
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;" value="">35</option>
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;" value="">40</option>
                                         </select>
                                     </div>
                                 </div>
@@ -97,11 +96,11 @@
                                 <div class="items-center">
                                     <div class="s-input mr-3">
                                         <select
-                                            class="select-bar w-[190px] px-2.5 bg-transparent leading-[26px] border h-auto pl-[15px] pr-[30px] py-0 rounded-[50px] border-solid border-[rgba(0,0,0,0.1)]">
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;">Select Country</option>
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;" value="">India</option>
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;" value="">Japan</option>
-                                            <option class="text-[#222] leading-5 px-2.5 py-0;" value="">England</option>
+                                            class="select-bar w-[190px] dark:bg-[#262950] dark:text-white px-2.5 bg-transparent leading-[26px] border h-auto pl-[15px] pr-[30px] py-0 rounded-[50px] border-solid border-[rgba(0,0,0,0.1)]">
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;">Select Country</option>
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;" value="">India</option>
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;" value="">Japan</option>
+                                            <option class="ext-[#222] dark:text-white  dark:bg-[#262950]  leading-5 px-2.5 py-0;" value="">England</option>
                                         </select>
                                     </div>
                                 </div>

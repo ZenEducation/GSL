626613a94c0d6b75502213614fe54d3695ce3ea7
kishan kantariya <kishankantariya111@gmail.com>
2023-07-06 15:25:08 +0530

    changed path of config file in app.vue

--- app.vue
+++ app.vue
@@ -52,7 +52,7 @@ useHead({
 
 <script setup>
 import { Amplify, Auth } from "aws-amplify";
-import awsconfig from '@/src/aws-exports';
+import awsconfig from '@/aws-exports';
 Amplify.configure(awsconfig);
 Auth.configure(awsconfig)
 </script>
